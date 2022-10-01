64df2ac4836730b3b246903da7a203379541a4ca
ArisHadisopiyan <aris.hadisopiyan@gmail.com>
2022-10-01 19:52:54 +0700

    a

--- New folder/assets copy/js/btn-back-to-top.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('.change-img')
-        .mouseover(function () {
-        $(this).attr("src", "assets/images/svg/arrow-up-circle-fill-omeans-6-color-with-logo-plus-9-color.svg");
-    })
-        .mouseout(function () {
-        $(this).attr("src", "assets/images/svg/arrow-up-circle-fill-omeans-6-color.svg");
-    });
-});
--- New folder/assets copy/js/check-sticky.js
+++
@@ -1,7 +0,0 @@
-const el = document.querySelector(".sticky-top")
-const observer = new IntersectionObserver( 
-  ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
-  { threshold: [1] }
-);
-
-observer.observe(el);
