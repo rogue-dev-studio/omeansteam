789f1051fb44116e395a687555c75ee93c89b76f
Omeans Team <47584746+omeansteam@users.noreply.github.com>
2023-10-13 07:44:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,11 @@ $request = $_SERVER['REQUEST_URI'];
 $viewDir = '/omeansteam/omeansteam.github.io';
 
 switch ($request) {
-    case '':
     case '/':
-        require __DIR__ . $viewDir . '/components/views/index.php';
+        require  . $viewDir . '/components/views/index.php';
         break;
     default:
         http_response_code(404);
-        require __DIR__ . $viewDir . '/components/views/empty/404.php';
+        require  . $viewDir . '/components/views/empty/404.php';
         // require __DIR__ . $viewDir . 'index.php';
 }
